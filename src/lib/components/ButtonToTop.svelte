8a517fa0970b89d37a68b36ea221b81669be0416
Rizki Ardian <radeite8@gmail.com>
2023-01-07 21:13:51 +0700

    Add scroll to top button

---
+++ src/lib/components/ButtonToTop.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import IconoirArrowUp from '~icons/iconoir/arrow-up'
+	import { Button } from 'flowbite-svelte'
+
+	export let visible = false
+
+	$: css = visible ? 'visible opacity-100 translate-y-0' : 'invisible opacity-0 translate-y-10'
+</script>
+
+<Button class="fixed bottom-4 right-4 !p-2 transition-all {css}" color="light" size="xl" on:click>
+	<IconoirArrowUp />
+</Button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '$lib/app.postcss'
+	import ButtonToTop from '$lib/components/ButtonToTop.svelte'
 	import Header from '$lib/components/Header.svelte'
 	import Meta from '$lib/components/Meta.svelte'
 	import Sidebar from '$lib/components/Sidebar.svelte'
@@ -9,12 +10,25 @@
 
 	export let data: LayoutData
 	$routes = data.routes
+
+	let root: HTMLElement
+	let scrolled = false
+
+	function setScrolled() {
+		scrolled = root.scrollTop > 720
+	}
+
+	function scrollToTop() {
+		root.scrollTo({ top: 0, behavior: 'smooth' })
+	}
 </script>
 
 <Meta />
 
 <div
 	class="h-full bg-white dark:bg-gray-900 dark:text-white overflow-x-hidden overflow-y-auto transition-none"
+	bind:this={root}
+	on:scroll={setScrolled}
 >
 	<Header />
 	<Sidebar />
@@ -28,4 +42,6 @@
 			<slot />
 		</div>
 	</div>
+
+	<ButtonToTop visible={scrolled} on:click={scrollToTop} />
 </div>
