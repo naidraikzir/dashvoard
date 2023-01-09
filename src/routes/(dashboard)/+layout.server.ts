ef942b4421f410b5519866b68921036ebfc749f5
Rizki Ardian <radeite8@gmail.com>
2023-01-09 09:19:38 +0700

    Move prerender flag to root layout

--- src/routes/(dashboard)/+layout.server.ts
+++ src/routes/(dashboard)/+layout.server.ts
@@ -1,7 +1,5 @@
 import type { LayoutServerLoad } from './$types'
 
-export const prerender = true
-
 export const load: LayoutServerLoad = async () => {
 	const modules = import.meta.glob('./**/+page.svelte')
 	const routes = new Set(
