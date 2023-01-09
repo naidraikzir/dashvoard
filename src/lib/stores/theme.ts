ce1a7b41f7a11297eaeb786c2b5d422542f81322
Rizki Ardian <radeite8@gmail.com>
2023-01-09 08:50:21 +0700

    Fix theme store

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -1,9 +1,11 @@
 import { writable } from 'svelte/store'
 import { browser } from '$app/environment'
 
-const stored = browser ? localStorage.theme : undefined
+type Theme = 'light' | 'dark'
 
-export const theme = writable(stored)
+const stored = browser ? (localStorage.theme || (window.matchMedia('(prefers-color-scheme: dark)').matches && 'dark')) : undefined
+
+export const theme = writable(<Theme>stored)
 
 theme.subscribe((value) => {
   if (browser) {
