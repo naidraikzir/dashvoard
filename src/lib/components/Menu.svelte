3febc2fe919b925596930f5b19eda4d503bd0a34
Rizki Ardian <radeite8@gmail.com>
2023-12-27 12:46:21 +0700

    Upgrade to Sveltekit 2

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,28 +1,32 @@
 <script lang="ts">
-	import { SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte'
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte'
 	import { page } from '$app/stores'
 	import menu from '$lib/fixtures/menu'
 
 	$: isMenuItemActive = (url: string) =>
 		(url === '/' && $page.url.pathname === url) ||
 		(url !== '/' && $page.url.pathname.startsWith(url))
+
+	$: activeUrl = $page.url.pathname
 </script>
 
-<SidebarWrapper class="!px-0">
-	<SidebarGroup class="!space-y-0">
-		{#each menu as { url, label, icon }}
-			<SidebarItem
-				{label}
-				href={url}
-				active={isMenuItemActive(url)}
-				class="rounded-none px-4 py-3 hover:bg-gray-200 transition {!isMenuItemActive(url) &&
-					'!text-gray-500 !dark:text-gray-400'}"
-				on:click
-			>
-				<svelte:fragment slot="icon">
-					<svelte:component this={icon} />
-				</svelte:fragment>
-			</SidebarItem>
-		{/each}
-	</SidebarGroup>
-</SidebarWrapper>
+<Sidebar {activeUrl}>
+	<SidebarWrapper class="!px-0">
+		<SidebarGroup class="!space-y-0">
+			{#each menu as { url, label, icon }}
+				<SidebarItem
+					{label}
+					href={url}
+					active={isMenuItemActive(url)}
+					class="rounded-none px-4 py-3 hover:bg-gray-200 transition {!isMenuItemActive(url) &&
+						'!text-gray-500 !dark:text-gray-400'}"
+					on:click
+				>
+					<svelte:fragment slot="icon">
+						<svelte:component this={icon} />
+					</svelte:fragment>
+				</SidebarItem>
+			{/each}
+		</SidebarGroup>
+	</SidebarWrapper>
+</Sidebar>
